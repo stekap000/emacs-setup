99e09a105bdb579269623e813d653b8a59a07bb8
stekap000 <stekap000@gmail.com>
2024-10-01 22:40:05 +0200

    case indentation withing switch for c

--- init.el
+++ init.el
@@ -59,6 +59,7 @@
 	(setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
 	(setq tab-width 4)
 	(setq indent-tabs-mode t)  ; use spaces only if nil
+	(c-set-offset 'case-label '+) ; use case that is indented within switch
  )
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
 
