5230aaeaadd472ec0845b3d25878f1858eba1346
stekap000 <stekap000@gmail.com>
2025-05-02 15:56:16 +0200

    small change

--- init.el
+++ init.el
@@ -70,16 +70,23 @@
 ;; Set up key bindings.
 (global-set-key (kbd "C-.") 'compile)
 
+;; Don't load welcome screen.
+(setq inhibit-startup-screen t)
+
+;; Set default directory.
+(setq default-directory "D:/github/")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(gruber-darker))
- ;;'(custom-enabled-themes '(naysayer))
  '(custom-safe-themes
-   '("e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7" "18d1131ad6ff8e8e34287d6de9299faba4f1b03874278463fb6f38e3abe65bdc" default))
- '(package-selected-packages '(company gruber-darker-theme zig-mode naysayer-theme)))
+   '("e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7"
+	 "18d1131ad6ff8e8e34287d6de9299faba4f1b03874278463fb6f38e3abe65bdc"
+	 default))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
